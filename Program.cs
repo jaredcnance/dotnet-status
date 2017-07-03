73b0c9d3028182735b6a75bd72d0038dae656f15
jaredcnance <jaredcnance@gmail.com>
2017-07-03 12:43:51 -0500

    add iis

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace DotnetHealth
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
     }
